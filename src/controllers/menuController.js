204cd16d90a0a21a31197630e48fbc1677362796
anwesha23032005 <anweshadey2005@gmail.com>
2026-09-10 20:02:47 +0530

    more buggsss

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -72,7 +72,7 @@ const createMenuItem = async (req, res) => {
 
     if (req.file && req.file.path) {
       // Image file uploaded via Multer + Cloudinary
-      imageUrl =req.file.filename;
+      imageUrl =req.file.path;
     } else if (req.body && req.body.Image) {
       // Direct image URL string passed (e.g., external API image link)
       imageUrl = req.body.Image;
