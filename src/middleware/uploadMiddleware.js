1f23251ec57e5ebebedbfee25f60c994cdebe4f7
anwesha23032005 <anweshadey2005@gmail.com>
2026-09-10 20:31:59 +0530

    make some changes

--- src/middleware/uploadMiddleware.js
+++ src/middleware/uploadMiddleware.js
@@ -1,16 +1,15 @@
 const multer = require("multer");
-const path = require("path");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("./cloudinary");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "restaurant-menu",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
   },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    // Added path.extname so images keep their .jpg/.png extension
-    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-  }
 });
 
 const upload = multer({ storage: storage });
+
 module.exports = upload;
